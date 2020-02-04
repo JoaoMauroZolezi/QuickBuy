7115c344dc31b3936f2f16ab77fe7cf61f69ee69
jonny zolezi <joao.zolezi@gmail.com>
2020-02-04 17:15:30 -0300

    Criacao das Entidades

---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,32 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public  class FormaPagamento
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool EhBoleto
+        {
+            get { return id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+        public bool EhCartaoCredito
+        {
+            get { return id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+        public bool EhDeposito
+        {
+            get { return id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+        public bool EhNaoDefinido
+        {
+            get { return id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+
+
+    }
+}
